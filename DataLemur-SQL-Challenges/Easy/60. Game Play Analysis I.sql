e88e3cfbcb66dec9116686dfd793ccb516605870
Sai Sahana Bhargavi Byrapu <101760308+SahanaByrapu@users.noreply.github.com>
2025-08-12 16:21:32 -0700

    Update 60. Game Play Analysis I.sql

--- DataLemur-SQL-Challenges/Easy/60. Game Play Analysis I.sql
+++ DataLemur-SQL-Challenges/Easy/60. Game Play Analysis I.sql
@@ -1,4 +1,6 @@
 -- Game Play Analysis I.
 -- https://leetcode.com/problems/game-play-analysis-i/description/
 
-
+SELECT player_id, MIN(event_date) AS first_login
+FROM Activity
+GROUP BY player_id;
